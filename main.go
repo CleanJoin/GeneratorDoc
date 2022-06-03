6035a133abf4498c292713edff8dd9d2e87c4d13
Sergey <80034792+CleanJoin@users.noreply.github.com>
2022-06-04 01:37:18 +0300

    refactor endpoints (downloads file)

--- main.go
+++ main.go
@@ -2,30 +2,11 @@ package main
 
 import (
 	"generatordoc/restapi"
-	"github.com/joho/godotenv"
-	"log"
 )
-
-func init() {
-	// loads values from .env into the system
-	if err := godotenv.Load(); err != nil {
-		log.Print("No .env file found")
-	}
-}
 
 func main() {
 
-	//fileUrl := "https://sycret.ru/service/apigendoc/forma_025u.xml"
-	//err := restapi.DownloadFile("restapi/docs/forma_025u.xml", fileUrl)
-	//if err != nil {
-	//	panic(err)
-	//} else {
-	//	fmt.Println("Downloaded: " + fileUrl)
-	//	restapi.ParserXml("restapi/docs/forma_025u.xml")
-	//}
-
-	chatServerGin := restapi.NewChatServerGin("localhost", 8888)
-
+	chatServerGin := restapi.NewChatServerGin("localhost", 8080)
 	chatServerGin.Use()
 	chatServerGin.Run()
 }
--- restapi/serverApi.go
+++ restapi/serverApi.go
@@ -1,15 +1,9 @@
 package restapi
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"io/ioutil"
-	"log"
 	"net/http"
-	"strings"
-	"time"
 )
 
 type ChatServerGin struct {
@@ -17,12 +11,6 @@ type ChatServerGin struct {
 	host            string
 	port            int
 	maxLastMessages uint
-}
-
-type ApiGenDoc struct {
-	Use      string `json:"Use"`
-	Text     string `json:"Text"`
-	RecordID string `json:"RecordID"`
 }
 
 type GenDoc struct {
@@ -47,12 +35,13 @@ func NewChatServerGin(host string, port int) *ChatServerGin {
 }
 
 func (chat *ChatServerGin) Use() {
-
 	chat.router = gin.Default()
 	// Конфигурируем все эндпоинты
-	chat.router.POST("/apigendoc", docHandler())
-	chat.router.POST("/gendoc", createrGenDocHandler())
-	chat.router.GET("/health", heathHandler())
+	chat.router.POST("/gendoc", createGenDocHandler())
+	chat.router.GET("/download/:filename", func(ctx *gin.Context) {
+		fileName := ctx.Param("filename")
+		ctx.FileAttachment("./restapi/docs/"+fileName, fileName)
+	})
 }
 
 func (chat *ChatServerGin) Run() error {
@@ -62,68 +51,10 @@ func (chat *ChatServerGin) Run() error {
 	return chat.router.Run()
 }
 
-func heathHandler() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		ctx.IndentedJSON(http.StatusOK, gin.H{
-			"success": true,
-			"time":    time.Now().Format(time.RFC3339),
-		})
-
-	}
-}
-
-func docHandler() gin.HandlerFunc {
-	return func(ctx *gin.Context) {
-		requestUser := new(ApiGenDoc)
-
-		statusCode, ctx2, checkBadRequest := validateClientRequest(ctx, requestUser)
-
-		if !checkBadRequest {
-			ctx.IndentedJSON(statusCode, ctx2)
-			return
-		}
-
-		jsonValue2, _ := json.Marshal(requestUser)
-		responseBody := bytes.NewBuffer(jsonValue2)
-		//strings.NewReader(`{"Use":"text_from_use_tag","Text": "data_from_text_tag","RecordID":"30"}`)
-		req, err := http.NewRequest("POST", "https://sycret.ru/service/apigendoc/apigendoc", responseBody)
-		if err != nil {
-			log.Fatal("Error reading request. ", err)
-		}
-		// Set headers
-		req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-		//req.Header.Set("Host", "sycret.ru")
-		req.Header.Set("User-Agent", "python-requests/2.27.1")
-		// Create and Add cookie to request
-		//	cookie := http.Cookie{Name: "cookie_name", Value: "cookie_value"}
-		//	req.AddCookie(&cookie)
-
-		// Set client timeout
-		client := &http.Client{Timeout: time.Second * 10}
-
-		// Send request
-		resp, err := client.Do(req)
-		if err != nil {
-			log.Fatal("Error reading response. ", err)
-		}
-		defer resp.Body.Close()
-
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			log.Fatal("Error reading body. ", err)
-		}
-		satrinout := strings.TrimSpace(string(body))
-		//satrinout = strings.Replace(satrinout, `\`, "", -1)
-
-		ctx.IndentedJSON(http.StatusCreated, satrinout)
-	}
-
-}
-
-func createrGenDocHandler() gin.HandlerFunc {
+func createGenDocHandler() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		requestUser := new(GenDoc)
-
+		var fileDownloadName string
 		statusCode, ctx2, checkBadRequest := validateClientRequest(ctx, requestUser)
 
 		if !checkBadRequest {
@@ -131,16 +62,20 @@ func createrGenDocHandler() gin.HandlerFunc {
 			return
 		}
 
-		ctx.IndentedJSON(http.StatusOK, gin.H{"URLWord": "your_url\\2022-05-26 14-12-04.doc", "URLPDF": " your_url\\2022-05-26 14-12-04.pdf"})
+		err := DownloadXMLFile("restapi/docs/forma_025u.xml", requestUser.URLTemplate)
+		if err != nil {
+			ctx.IndentedJSON(http.StatusBadRequest, gin.H{"error": fmt.Sprintf("Не скачался фаил %s", requestUser.URLTemplate)})
+			return
+		} else {
+			fileDownloadName = AddValueInXML("restapi/docs/forma_025u.xml")
+		}
+		ctx.IndentedJSON(http.StatusOK, gin.H{"URLWord": fmt.Sprintf("http://localhost:8080/download/%s.doc", fileDownloadName), "URLPDF": fmt.Sprintf("http://localhost:8080/download/%s.pdf", fileDownloadName)})
 	}
-
 }
 
 func validateClientRequest(ctx *gin.Context, requestData interface{}) (int, interface{}, bool) {
-
 	err := ctx.BindJSON(&requestData)
 	if err != nil {
-
 		return http.StatusBadRequest, gin.H{"error": "Не содержит поля в запросе"}, false
 	}
 	return http.StatusOK, gin.H{"error": ""}, true
